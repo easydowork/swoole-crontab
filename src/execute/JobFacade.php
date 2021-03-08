727b1148abbc2de26698b7340cc2f31170abb7ca
lyl <254364502@qq.com>
2021-03-08 10:48:29 +0800

    添加注释

--- src/execute/JobFacade.php
+++ src/execute/JobFacade.php
@@ -21,24 +21,36 @@ class JobFacade
      */
     public static function getExecute($key,&$data)
     {
+        //判断是否为关闭状态
+        if(empty($data['status'])) {
+            return;
+        }
 
+        //判断执行开始时间
         if($data['start_time'] && $data['start_time'] < time()){
             return;
         }
 
+        //判断执行结束时间
         if($data['stop_time'] && $data['stop_time'] < time()){
             return;
         }
 
+        //判断任务执行文件
         $executeClass = Config::getInstance()->jobConfig['run_types'][$data['run_type']]??'';
-
         if(empty($executeClass))
             return;
 
-        $times = FormatParser::getInstance()->parse($data['format']);
-
-        if(empty($times))
+        //获取1分钟内执行的时间戳
+        try {
+            $times = FormatParser::getInstance()->parse($data['format']);
+            if(empty($times))
+                return;
+        }catch (\Exception $e){
+            $data['status'] = 0;
+            Logger::getInstance()->info('执行定时任务['.$data['name'].'],解析[format]失败.');
             return;
+        }
 
         $now = time();
 
@@ -48,7 +60,8 @@ class JobFacade
                 $t = 0.001;
             }
 
-            $timeId = Timer::after($t*1000,function () use($executeClass,$data){
+            //加入定时任务
+            Timer::after($t*1000,function () use($executeClass,$data){
                 try{
                     /** @var $execute JobExecute */
                     $execute = new $executeClass();
@@ -58,7 +71,7 @@ class JobFacade
                         Logger::getInstance()->info('执行定时任务['.$data['name'].'],返回结果成功.');
                     }
                 }catch (\Exception $e){
-                    Logger::getInstance()->error('执行定时任务异常['.$data['name'].']'.$e->getMessage());
+                    Logger::getInstance()->error('执行定时任务['.$data['name'].'],异常:'.$e->getMessage());
                 }
             });
         }
