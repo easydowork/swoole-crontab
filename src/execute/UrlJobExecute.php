1b663f1126f7bc96a4c7260fd633ce2850bc3c1d
lyl <254364502@qq.com>
2021-03-08 11:37:17 +0800

    定时任务执行 url修改

--- src/execute/UrlJobExecute.php
+++ src/execute/UrlJobExecute.php
@@ -18,14 +18,16 @@ class UrlJobExecute extends JobExecute
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $data['command']);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_NOBODY, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); //返回状态码
         curl_close($ch);
-
-       return $httpCode==200;
+        return $httpCode==200;
     }
 }
